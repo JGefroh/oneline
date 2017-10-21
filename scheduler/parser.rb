6d632d3d003f096f992e4da9f041a7adb13691be
Joseph Gefroh <joseph.gefroh@gmail.com>
2017-10-21 08:15:42 -0700

    Return gracefully if component is missing.

--- scheduler/parser.rb
+++ scheduler/parser.rb
@@ -5,24 +5,28 @@ module Scheduler
     end
 
     private def to_data(text)
+      #<X> <on|at|in> <|time-period|time|date|day>
       data = {}
       last_operator_index = text.index(' on ') || text.index(' at ') || text.index(' in ')
       data[:label] = text
-      data[:time] = text
-      "<X> <on|at|in> <|time-period|time|date|day>"
+      data[:time] = identify_time(text)
+      data[:day] = identify_day(text)
       return data
     end
 
     def identify_time(text)
-      return text.scan(/([0-9]{1,2}:?[0-9]{0,2}(pm|am))/)[0][0]
+      matches = text.scan(/([0-9]{1,2}:?[0-9]{0,2}(pm|am))/)
+      return matches[0][0] unless matches.empty?
     end
 
     def identify_day(text)
-      return text.scan(/(monday|mon|tuesday|tue|wednesday|wed|thursday|thu|friday|fri|saturday|sat|sunday|sun)/).first.first
+      matches = text.scan(/(monday|mon|tuesday|tue|wednesday|wed|thursday|thu|friday|fri|saturday|sat|sunday|sun)/)
+      return matches[0][0] unless matches.empty?
     end
 
     def identify_date(text)
-      return text.scan(/(jan|feb|mar|apr|may|june|july|aug|sep|oct|nov|dec)(\s[0-9]{1,2}(th|nd|rd|){0,1})?/)
+      matches = text.scan(/(jan|feb|mar|apr|may|june|july|aug|sep|oct|nov|dec)(\s[0-9]{1,2}(th|nd|rd|){0,1})?/)
+      return matches[0][0] unless matches.empty?
     end
   end
 end
