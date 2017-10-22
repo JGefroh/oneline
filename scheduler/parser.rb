aac36179df3e16e828b8c5a20721ca6383228b40
Joseph Gefroh <joseph.gefroh@gmail.com>
2017-10-22 16:04:24 -0700

    Removed duplicate code.

--- scheduler/parser.rb
+++ scheduler/parser.rb
@@ -23,56 +23,39 @@ module Scheduler
       return data
     end
 
-    def identify_and_store(field, data, text)
+    private def identify_and_store(field, data, text)
       if field === :label
         label = identify_label(text)
         data[:label] = label
       else
-        identified_field = send("identify_#{field}", text)
+        identified_field = send(:identify_field, field, text)
         store(identified_field, data, field)
       end
     end
 
-    def identify_time(text)
-      matches = text.scan(TIME_REGEX)
-      first_match_index = text.index(TIME_REGEX)
-      return {time: matches[0][0], index: first_match_index} unless matches.empty?
-    end
-
-    def identify_relative_day(text)
-      matches = text.scan(RELATIVE_DAY_REGEX)
-      first_match_index = text.index(/(today|tomorrow)/)
-      return {relative_day: matches[0][0], index: first_match_index} unless matches.empty?
-    end
-
-    def identify_day(text)
-      matches = text.scan(DAY_REGEX)
-      first_match_index = text.index(DAY_REGEX)
-      return {day: matches[0][0], index: first_match_index} unless matches.empty?
-    end
-
-    def identify_date(text)
-      matches = text.scan(DATE_REGEX)
-      first_match_index = text.index(DATE_REGEX)
-      return {date: matches[0][0], index: first_match_index} unless matches.empty?
+    def identify_field(field, text)
+      regex = self.class.const_get("#{field.to_s.upcase}_REGEX")
+      matches = text.scan(regex)
+      first_match_index = text.index(regex)
+      return {"#{field}": matches[0][0], index: first_match_index} unless matches.empty?
     end
 
     def identify_label(text)
       label = text.dup
 
-      date = identify_date(label)
+      date = identify_field(:date, label)
       date_article_offset = [' on ', ' at '].include?(label[date[:index] - 4, 4]) ? 4 : 0 unless date.nil?
       label.slice!(date[:index] - date_article_offset, date[:date].length + date_article_offset) unless date.nil?
 
-      relative_day = identify_relative_day(label)
+      relative_day = identify_field(:relative_day, label)
       relative_day_article_offset = label[relative_day[:index] - 4, 4] === ' on ' ? 4 : 0 unless relative_day.nil?
       label.slice!(relative_day[:index] - relative_day_article_offset, relative_day[:relative_day].length + relative_day_article_offset) unless relative_day.nil?
 
-      time = identify_time(label)
+      time = identify_field(:time, label)
       time_article_offset = label[time[:index] - 4, 4] === ' at ' ? 4 : 0 unless time.nil?
       label.slice!(time[:index] - time_article_offset, time[:time].length + time_article_offset) unless time.nil?
 
-      day = identify_day(label)
+      day = identify_field(:day, label)
       day_article_offset = label[day[:index] - 4, 4] === ' on ' ? 4 : 0 unless day.nil?
       label.slice!(day[:index] - day_article_offset, day[:day].length + day_article_offset) unless day.nil?
 
