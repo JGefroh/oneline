fa164888523c448597d5c9c71608525d35a3e821
Joseph Gefroh <joseph.gefroh@gmail.com>
2017-10-21 20:41:35 -0700

    Add help instructions and basic commands.

--- main.rb
+++ main.rb
@@ -1,18 +1,47 @@
 require_relative 'scheduler/processor'
 
-tasks = []
-processor = Scheduler::Processor.new
+class Main
+  attr_accessor :tasks
+  def initialize
+    @tasks = []
+  end
 
-input = nil
+  def start
+    processor = Scheduler::Processor.new
 
-while input != 'exit'
-  input = gets
-  task = processor.process(input)
-  puts task
-  
-  if task[:interpreted]
-    tasks << task
-    puts "Your list.."
-    tasks.each{ |task| puts "* #{task[:original_text]}"}
+    input = ''
+
+    puts "Hi, I'm your personal assistant. Type 'help me' to see what I can do!"
+    while input.strip != 'exit'
+      print "> "
+      input = gets
+      task = processor.process(input)
+
+      print_list if input.strip === 'list'
+      print_help if input.strip === 'help' || input.strip === 'help me'
+
+      if task[:interpreted]
+        @tasks << task
+        puts "----- Great! I'll remind you about this on #{task[:date]} at #{task[:time].strftime('%l:%M %P').strip}." if !task[:date].nil? && !task[:time].nil?
+        puts "----- Great! I'll remind you about this on #{task[:date]}." if !task[:date].nil? && task[:time].nil?
+        puts "----- Great! I'll remind you about this today at #{task[:time].strftime('%l:%M %P').strip}." if task[:date].nil? && !task[:time].nil?
+      end
+    end
+  end
+
+  def print_help
+    puts "Ask me to remember something and I will."
+    puts "eg. 'Go to the movies at 2pm tomorrow.'"
+    puts ""
+    puts "Type 'list' to see everything I'm remembering at the moment."
+    puts "Type 'exit' to quit."
+  end
+
+  def print_list
+    puts "----- Your list..\n"
+    @tasks.each{ |task| puts "----- * #{task[:original_text]}"}
   end
 end
+
+m = Main.new
+m.start
---
+++ notifier/notifier.rb
@@ -0,0 +1,9 @@
+require 'date'
+require 'time'
+
+module Notifier
+  class Notifier
+    def notify(target, message, type)
+    end
+  end
+end
