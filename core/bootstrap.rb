28426b328c8437d15a04ca3fdb1bcbcad1bea4c0
Joseph Gefroh <joseph.gefroh@gmail.com>
2017-10-24 21:50:12 -0700

    Automatically detect tests.

--- core/bootstrap.rb
+++ core/bootstrap.rb
@@ -2,16 +2,13 @@ require 'find'
 
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__))) unless $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__)))
 
-if ARGV[0] === 'test'
-  require './test'
-else
-  Find.find('./') do |path|
-    if path =~ /plugin\.rb/
-      if path != File.expand_path(File.dirname(__FILE__))
-        require "#{path}"
-      end
+load_regex = ARGV[0] === 'test' ? /test\.rb/ : /plugin\.rb/
+Find.find('./') do |path|
+  if path =~ load_regex
+    if path != File.expand_path(File.dirname(__FILE__))
+      require "#{path}"
     end
   end
-
-  require_relative './application'
 end
+
+require_relative './application' if ARGV[0] != 'test'
--- test.rb
+++
@@ -1,3 +0,0 @@
-require './plugins/scheduler/tests/interpreter_test'
-require './plugins/scheduler/tests/scheduled_item_test'
-require './plugins/scheduler/tests/parser_test'
