2817aff8dc87df6573f2738bfc919cd7ca0727a0
Joseph Gefroh <joseph.gefroh@gmail.com>
2017-10-26 14:44:18 -0700

    WIP - Use Vue instead of react.

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,40 +1,24 @@
-console.info("Hello")
-
 var messages = [
-  {key: 1, message: 'Hello OneLine!', sender: 'you'},
-  {key: 2, message: 'The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.', sender: 'system'}
+  {message: 'hello', sender: 'you'},
+  {message: 'hi Joseph!', sender: 'system'}
 ]
-
-var Message = React.createClass({
-  propTypes: {
-    message: React.PropTypes.string.isRequired
+var app = new Vue({
+  el: '#app',
+  data: {
+    messages: messages,
+    newMessage: ''
   },
-  render: function() {
-    return React.createElement('div', {className: `Message-wrapper Message-wrapper-${this.props.sender === 'system' ? 'received' : 'sent'}`}, React.createElement('div', {className: `Message Message-${this.props.sender === 'system' ? 'received' : 'sent'}`}, this.props.message));
-  }
-});
-
-
-var ChatInput = React.createClass({
-  propTypes: {
-  },
-  render: function() {
-    return React.createElement('div', {},
-      React.createElement('input', {}),
-      React.createElement('button', {}, 'Send'),
-    );
+  methods: {
+    sendMessage: function(message) {
+      if (message) {
+        messages.push({
+          message: message,
+          sender: 'you'
+        });
+        this.newMessage = null;
+        messageWindow = this.$el.querySelector("#message-window");
+        messageWindow.scrollTop = messageWindow.scrollHeight;
+      }
+    }
   }
 })
-
-var messageElements = messages.map(function(message) {
-  return React.createElement(Message, message);
-});
-var chatMessagesElement =
-  React.createElement('div', {},
-    React.createElement('h1', {}, 'OneLine'),
-    React.createElement('div', {}, messageElements)
-  )
-var chatInputElement = React.createElement(ChatInput, {})
-
-ReactDOM.render(chatMessagesElement, document.getElementById('chat-messages'))
-ReactDOM.render(chatInputElement, document.getElementById('chat-input'))
