712faca19de4e98eb0c85e851b3b9ca000f7ca4b
Joseph Gefroh <joseph.gefroh@gmail.com>
2017-10-27 20:30:55 -0700

    Changed unused variable to used variable.

--- lib/oneline/server/incoming_sms_handler.rb
+++ lib/oneline/server/incoming_sms_handler.rb
@@ -1,13 +1,13 @@
 module Server
   class IncomingSmsHandler
     def handle(payload)
-      verify_source(request)
+      verify_source(payload)
       plugin_responses = Core::Plugin.call_all(payload[:Text] || payload['Text'], {owner_id: payload[:From] || payload['From']})
       send_plugin_responses_as_texts(plugin_responses, payload[:From] || payload['From'])
       return true
     end
 
-    private def verify_source(request)
+    private def verify_source(payload)
       raise 'unauthorized' unless payload['key'] && payload['key'] === ENV['SMS_WEBHOOK_KEY']
     end
 
