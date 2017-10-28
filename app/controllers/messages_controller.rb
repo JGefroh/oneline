de3e36d96c18187db16ebbc3bb25f9a5ee670d85
Joseph Gefroh <joseph.gefroh@gmail.com>
2017-10-28 12:22:44 -0700

    Set time zone from texts too.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,8 +2,9 @@ class MessagesController < ApplicationController
   around_action :set_time_zone
 
   def set_time_zone
-    if !params[:owner_id].nil? && params[:owner_id].length > 0
-      user_time_zone = User.find_or_create_by(user_identifier: params[:owner_id]).time_zone
+    owner_id = params[:owner_id] || payload[:From] || payload['From']
+    if !owner_id.nil? && owner_id.length > 0
+      user_time_zone = User.find_or_create_by(user_identifier: owner_id).time_zone
       user_time_zone = Time.zone unless ActiveSupport::TimeZone[user_time_zone].present?
       Time.use_zone(user_time_zone || Time.zone) {
         puts "User TZ: #{user_time_zone}. Actual TZ: #{Time.zone}. Request time: #{Time.zone}"
