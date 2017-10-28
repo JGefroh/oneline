daa4bf48b2284a7693f985faac8dd8c25fe73bff
Joseph Gefroh <joseph.gefroh@gmail.com>
2017-10-27 20:26:17 -0700

    SMS endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'application#index'
   scope '/api' do
-    resource :messages, only: [:create]
-    post 'sms', on: :collection
+    resource :messages, only: [:create] do
+      post 'sms', on: :collection
+    end
   end
 end
