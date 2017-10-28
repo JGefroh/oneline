6501f263c407e640fd7255086306a54f70f7aefc
Joseph Gefroh <joseph.gefroh@gmail.com>
2017-10-27 19:51:10 -0700

    Update port.

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: bundle exec rails s
+web: bundle exec rails s -p 5001
 worker: bundle exec rake jobs:work
--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 if (location.href.indexOf('127.0.0.1') >= 0) {
-  axios.defaults.baseURL = 'http://127.0.0.1:5000/api'
+  axios.defaults.baseURL = 'http://127.0.0.1:5001/api'
 }
 else {
   axios.defaults.baseURL = 'http://oneline.jgefroh.com/api'
