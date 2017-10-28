4f3d1ddd7d8c3b64c494fd00885d40f741bb9534
Joseph Gefroh <joseph.gefroh@gmail.com>
2017-10-27 20:22:22 -0700

    Enable SMS endpoint.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'application#index'
   scope '/api' do
-    resource :messages, only: [:create] do
-      post 'sms', on: :collection
-    end
+    resource :messages, only: [:create]
+    post 'sms', on: :collection
   end
 end
