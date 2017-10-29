2a7f39ab31128a701cf3a4babacbe4af315fc0b5
Joseph Gefroh <joseph.gefroh@gmail.com>
2017-10-29 05:45:01 -0700

    Reminder has timestamp.

--- app/jobs/send_reminder_job.rb
+++ app/jobs/send_reminder_job.rb
@@ -11,8 +11,11 @@ class SendReminderJob < ApplicationJob
     list_item = ListItem.find_by(id: params[:id])
     return unless list_item
     if list_item.notify? && list_item.user.mobile_phone_number_verified
+
+      date = list_item.date ? list_item.date : Date.today
+      time = list_item.time ? list_item.time.strftime('%l:%M %P %Z').strip : ''
       @notifiers.each { |notifier|
-        notifier.notify(list_item.user.mobile_phone_number, list_item.label)
+        notifier.notify(list_item.user.mobile_phone_number, "#{list_item.original_text} | (#{date.strftime('%m/%d ')}#{time})")
       }
       list_item.update(notified_at: DateTime.current)
     end
